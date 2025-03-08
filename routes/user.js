2eae19e0821482594abd7ee478b01520ee2e760c
RenierRains <spideypeter04@gmail.com>
2025-03-08 19:00:33 +0800

    user.js model quickfix

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const { verifyToken } = require('../controllers/authController');
 // PUT /api/user/profile-picture |to update profile pic
 router.put('/profile-picture', verifyToken, userController.uploadProfilePic, userController.updateProfilePicture);
 
-router.post('/fcm-token', userController.verifyToken, userController.updateFcmToken);
+// POST /api/user/fcm-token | to update FCM token
+router.post('/fcm-token', verifyToken, userController.updateFcmToken);
 
 module.exports = router;
